02ef2d78b5071007fb33a1bc987fa1e50f7f4dcf
Almir Loiola Bindo <hal0001@gmail.com>
2025-11-11 18:28:33 -0300

    capitulo-4-completo-add

--- src/main/java/com/algaworks/algafood/domain/repository/CidadeRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CidadeRepository.java
@@ -1,14 +1,14 @@
 package com.algaworks.algafood.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 import com.algaworks.algafood.domain.model.Cidade;
 
-public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+public interface CidadeRepository {
 	
-	/*
-	 * List<Cidade> listar(); Cidade buscar(Long id); Cidade salvar(Cidade cidade);
-	 * void remover(Long id);
-	 */
+	List<Cidade> listar();
+	Cidade buscar(Long id);
+	Cidade salvar(Cidade cidade);
+	void remover(Long id);
 
 }
